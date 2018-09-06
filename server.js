a252293d9b1e54b3e99637b87f171e54fe327d21
Tyler <tylerhines1992@gmail.com>
2018-09-06 13:15:16 -0400

    added post call in server.js

--- server.js
+++ server.js
@@ -22,3 +22,54 @@ app.listen( port, function() {
 app.get('/', function(req, res) {
 	res.send(application_root);
 })
+
+app.get('/transactions', function(req, res) {
+	return TransactionModel.find(function(err, transactions) {
+		if(!err) {
+			return res.send(transactions);
+		} else {
+			return console.log(err);
+		}
+	});
+});
+
+app.post('/transactions', function(req, res) {
+	var transaction = new TransactionModel({
+		source: req.body.source,
+		category: req.body.category,
+		amount: req.body.amount,
+		outflow: req.body.outflow
+	});
+
+	return transaction.save(function(err) {
+		if(!err) {
+			console.log('created');
+			return res.send(transaction);
+		} else {
+			console.log(err);
+		}
+	})
+})
+
+mongoose.connect('mongodb://localhost/library_database');
+
+var Transaction = new mongoose.Schema({
+	source: String,
+	category: String,
+	amount: Number,
+	outflow: Boolean
+});
+
+var TransactionModel = mongoose.model('Transaction', Transaction);
+
+app.configure(function() {
+	app.use(express.bodyParser());
+
+	app.use(express.methodOverride());
+
+	app.use(app.router);
+
+	app.use(express.static(path.join(application_root, 'site')));
+
+	app.use(express.errorHandler({ dumbExceptions: true, showStack: true}));
+})
